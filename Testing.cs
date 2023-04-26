f42f5bedf8cf1bffa2f99882d9345528b6185dce
SMLkaiellis08 <kaicycle11@gmail.com>
2023-04-26 14:31:06 +0100

    Name Change

--- Testing.cs
+++ Testing.cs
@@ -12,12 +12,12 @@ namespace MagmaMc.UAA.Test
         {
             var stopwatch = new Stopwatch();
 
-            stopwatch.Restart();
             string UserToken = UserData.GetToken((new APIData() { { "Username", "testaccount" }, { "Password", "testaccount" } }));
             string DevToken = "13d99018931c65a47902c1680f8e7981a8575991.8aedfbd3d5ed506a34602d29fbdfdfb8";
-            UserData.SetCustomData(DevToken, UserToken, "test.txt", "tes2");
-            Console.WriteLine(UserData.GetCustomData(DevToken, UserToken, "test.txt"));
-            Console.WriteLine($"Time taken: {stopwatch.ElapsedMilliseconds} ms");
+            stopwatch.Restart();
+            UserData.SetCustomData(DevToken, UserToken, "Time", $"Time taken: {stopwatch.ElapsedMilliseconds} ms");
+            UserData.SetCustomData(DevToken, UserToken, "Time", $"Time taken: {stopwatch.ElapsedMilliseconds} ms");
+            Console.WriteLine(UserData.GetCustomData(DevToken, UserToken, "Time"));
 
             stopwatch.Stop();
         }
